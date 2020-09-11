2fe8ba4636c7e816d0d645defdff1c9888766ae2
Paweł Szczepankiewicz <pawel.szczepankiewicz@enginiety.com>
2020-09-11 07:52:47 +0200

    First test of selenium

---
+++ SeleniumTesting/Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("http://test.lovi.pl/");
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            driver.FindElement(By.XPath("//li[@class='search']")).Click();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+
+            driver.FindElement(By.Name("search_engine")).SendKeys("butelka");
+
+            driver.FindElement(By.XPath("//button[@class='btn-primary']")).Click();
+        }
+    }
+}
