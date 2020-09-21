4b378a96302ea09fdf7257fe9267ec9897b2ff7c
Paweł Szczepankiewicz <pawel.szczepankiewicz@enginiety.com>
2020-09-21 09:27:03 +0200

    Added NUnit

---
+++ SeleniumTesting/NUnitTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumTesting
+{
+    [TestFixture]
+    [Parallelizable]
+    //[Ignore("Skip this tests")]
+    public class NUnitTests
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [SetUp]
+        public void SetUpTests()
+        {
+            driver.Manage().Window.Maximize();
+            WaitThree(driver);
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Navigate().GoToUrl("https://www.ivoclarvivadent.pl/pl/");
+            WaitThree(driver);
+
+            driver.FindElement(By.XPath("//b[@class='icon-lupe header-search-item-icon']")).Click();
+            WaitThree(driver);
+
+            driver.FindElement(By.Name("q")).SendKeys("zęby" + Keys.Enter);
+        }
+
+        [TearDown]
+        public void EndsTests()
+        {
+            driver.Quit();
+        }
+
+        public static void WaitThree(IWebDriver driver)
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+        }
+
+    }
+}
--- SeleniumTesting/Program.cs
+++ SeleniumTesting/Program.cs
@@ -9,32 +9,7 @@ namespace SeleniumTesting
     {
         static void Main(string[] args)
         {
-            IWebDriver driver = new ChromeDriver();
-
-            Start(driver);
+            
         }
-
-        public static void Start(IWebDriver driver)
-        {
-            driver.Manage().Window.Maximize();
-
-            driver.Navigate().GoToUrl("http://test.lovi.pl/");
-
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-
-            driver.FindElement(By.XPath("//li[@class='search']")).Click();
-
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
-
-            driver.FindElement(By.Name("search_engine")).SendKeys("butelka");
-
-            driver.FindElement(By.XPath("//button[@class='btn-primary']")).Click();
-        }
-
-        public static void End(IWebDriver driver)
-        {
-            driver.Close();
-        }
-
     }
 }
